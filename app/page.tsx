ac14e4e2f660ee6b1d833d35e82e8de5e1ab8bec
shakib <shakibur.rahman@egeneration.co>
2025-04-21 17:46:49 +0600

    dashboard design updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,30 @@
 import Image from "next/image";
+import TransactionChart from "./components/TransactionChart";
 
 export default function Home() {
   return (
-    <div className="border-2 border-gray-300 rounded-lg p-6 bg-gradient-to-r from-cyan-200 to-red-100 shadow-md">
-      <h1 className="text-2xl font-bold mb-4">Summary</h1>
-      <div className="flex justify-between">
-        <div className="mt-4 border-2 border-gray-400 rounded-lg p-3 space-y-2">
-          <p className="text-3xl font-bold text-green-600">$10,000.00</p>
-          <h2 className="text-xl font-semibold mb-2">Total Balance</h2>
+    <div>
+      <h2 className="text-2xl font-bold text-center mb-6">Summary Reports</h2>
+      
+      <div className="grid grid-cols-3 gap-4 mb-6">
+        <div className="p-4 bg-green-100 rounded-lg shadow text-center">
+          <p className="text-sm text-gray-600">Total Income</p>
+          <p className="text-xl font-bold text-green-600">$20,000.00</p>
         </div>
-        <div className="mt-4 border-2 border-gray-400 rounded-lg p-3 space-y-2">
-          <p className="text-3xl font-bold text-green-600">$5,000.00</p>
-          <h2 className="text-xl font-semibold mb-2">Total Income</h2>
+        <div className="p-4 bg-red-100 rounded-lg shadow text-center">
+          <p className="text-sm text-gray-600">Total Expenses</p>
+          <p className="text-xl font-bold text-red-600">$10,000.00</p>
         </div>
-        <div className="mt-4 border-2 border-gray-400 rounded-lg p-3 space-y-2">
-          <p className="text-3xl font-bold text-red-600">$2,000.00</p>
-          <h2 className="text-xl font-semibold mb-2">Total Expenses</h2>
+        <div className='p-4 bg-blue-100 rounded-lg shadow text-center'>
+          <p className="text-sm text-gray-600">Net Balance</p>
+          <p className="text-xl font-bold text-blue-600">
+            $10,000.00
+          </p>
         </div>
+      </div>
+
+      <div>
+        <TransactionChart />
       </div>
 
     </div>
